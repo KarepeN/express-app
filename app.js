db54bec1bce391705d45ecf8f8d541b71fef71c1
KarepeN <s540109@nwmissouri.edu>
2020-10-14 14:58:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require('express')
 const app = express()
 
 
-
 app.get('/', (req, res) => {
-    res.send(' Hello world ')
-  })
+  res.send(' Hello world ')
+})
 
 app.get('/test', (req, res) => {
-    res.send(' Hello world using test ')
-  })
+  res.send(' Hello world using test ')
+})
 app.get('/Nithya', (req, res) => {
-    res.send(' Hey,This is Nithya Karepe ')
-  })
-  
-  app.listen(3000, () => {
-    console.log(`Listening on port 3000!`)
-  })
+  res.send(' Hey,This is Nithya Karepe ')
+})
+
+app.listen(1408, () => {
+  console.log(`Listening on port 1408!`)
+})
